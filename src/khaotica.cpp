c181301fd827e9f950aff6f71baf07f75461715d
Sergey Rachev <sergey.rachev@gmail.com>
2015-08-06 22:53:56 +0600

    compilable

--- src/khaotica.cpp
+++ src/khaotica.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <string>
+#include <memory>
+
 #include <boost/uuid/uuid_io.hpp>
 #include <boost/make_shared.hpp>
 #include <boost/program_options.hpp>
@@ -5,14 +9,32 @@
 #include <boost/rational.hpp>
 #include <boost/limits.hpp>
 #include <boost/foreach.hpp>
-#include <iostream>
-#include <string>
-
 #include <boost/uuid/string_generator.hpp>
 #include <boost/thread/thread.hpp>
 #include <boost/thread/locks.hpp>
 #include <boost/algorithm/hex.hpp>
 #include <boost/chrono.hpp>
+
+
+#include <clang/AST/ASTConsumer.h>
+#include <clang/AST/ASTContext.h>
+#include <clang/AST/DeclVisitor.h>
+#include <clang/Basic/Builtins.h>
+#include <clang/Basic/Diagnostic.h>
+#include <clang/Basic/FileManager.h>
+#include <clang/Basic/IdentifierTable.h>
+#include <clang/Basic/SourceManager.h>
+#include <clang/Basic/TargetInfo.h>
+#include <clang/Frontend/CompilerInstance.h>
+#include <clang/Frontend/FrontendActions.h>
+#include <clang/Frontend/TextDiagnosticBuffer.h>
+#include <clang/Lex/HeaderSearch.h>
+#include <clang/Lex/Preprocessor.h>
+#include <clang/Parse/ParseAST.h>
+#include <clang/Parse/Parser.h>
+#include <clang/Sema/Sema.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/Support/Casting.h>
 
 bool process_arguments( int argc, char* argv[], const boost::program_options::options_description& _opt_desc, const boost::program_options::positional_options_description& _pos_opt_desc, boost::program_options::variables_map& varmap ) {
     namespace po = boost::program_options;
@@ -29,6 +51,8 @@ bool process_arguments( int argc, char* argv[], const boost::program_options::op
         std::cout << _opt_desc << std::endl;
         return false;
     }
+
+    return true;
 }
 
 int main( int argc, char* argv[] ) {
@@ -50,5 +74,19 @@ int main( int argc, char* argv[] ) {
         return 1;
     }
 
+    using namespace clang;
+    using namespace llvm;
 
+    auto compiler(std::make_shared<CompilerInstance>());
+    compiler->createDiagnostics(); // to stdout
+    assert(compiler->hasDiagnostics());
+    compiler->getFrontendOpts().Inputs.push_back({ "microuniverse.cpp", IK_CXX });
+
+    compiler->getFrontendOpts().OutputFile = "ast.xml";
+    compiler->getTargetOpts().Triple = "x86"; // x86, alpha, ppc, ppc64, ...
+    compiler->getLangOpts().CPlusPlus = 1;
+    std::shared_ptr<FrontendAction> action(std::make_shared<ASTPrintAction>());
+
+    bool actionSuccessful = compiler->ExecuteAction(*action);
+    assert(actionSuccessful);
 }
