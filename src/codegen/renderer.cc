b036b0fee6efc36a11b4b41adef3ac709c794604
Sergey Rachev <sergey.rachev@gmail.com>
2017-02-10 21:01:25 +0100

    // This is an open source non-commercial project. Dear PVS-Studio, please check it. // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com

--- src/codegen/renderer.cc
+++ src/codegen/renderer.cc
@@ -1,3 +1,6 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
 #include "llvm/Analysis/Passes.h"
 #include "llvm/ExecutionEngine/ExecutionEngine.h"
 #include "llvm/ExecutionEngine/MCJIT.h"
