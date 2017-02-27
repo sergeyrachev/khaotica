22f93a5b791ea3167653d20686eebc60a92bc8cf
Sergey Rachev <sergey.rachev@gmail.com>
2017-02-27 21:51:17 +0100

    Fix build for Windows with CMAKE;
    Usage: cmake -DLLVM_ROOT=d:\work\env\llvm\shared\x86\release\ -DBOOST_ROOT=d:\work\env\boost\boost_1_63_0\ ..\khaotica\
    
    README added

--- deps/boost-config.cmake
+++ deps/boost-config.cmake
@@ -1,8 +1,8 @@
 set(BOOST_ROOT "" CACHE PATH "")
-set(Boost_DEBUG ON)
+#set(Boost_DEBUG ON)
 set(Boost_USE_STATIC_LIBS ON)
 
-find_package(Boost 1.60 COMPONENTS program_options system)
+find_package(Boost 1.60 COMPONENTS program_options system date_time thread)
 
 if(Boost_FOUND)
     set(boost_COMPILE_DEFINITIONS ${Boost_LIB_DIAGNOSTIC_DEFINITIONS})
--- deps/llvm-config.cmake
+++ deps/llvm-config.cmake
@@ -4,7 +4,18 @@ find_package(LLVM 3.9.0 PATHS ${LLVM_ROOT})
 
 if(LLVM_FOUND)
     #all works bad in Windows-> LTO-NOTFOUND 
-    llvm_map_components_to_libnames(llvm_libs all)
+    llvm_map_components_to_libnames(llvm_libs analysis asmparser bitreader bitwriter codegen DebugInfoCodeView DebugInfoDWARF DebugInfoPDB Symbolize executionengine irreader Interpreter OrcJIT MCJIT RuntimeDyld Core  LibDriver LineEditor Linker MC MCDisassembler  MCParser Object ObjectYAML Option Passes ProfileData  Support  TableGen AArch64 AMDGPU ARM
+BPF
+Hexagon
+Mips
+MSP430
+NVPTX
+PowerPC
+Sparc
+SystemZ
+X86
+XCore 
+)
 
     set(llvm_INCLUDE_DIRECTORIES ${LLVM_INCLUDE_DIRS})
     set(llvm_LIBRARIES ${llvm_libs})
