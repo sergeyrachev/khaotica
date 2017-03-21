d5a3f593322fe21d8d9542387299dc3840469b0b
Sergey Rachev <sergey.rachev@gmail.com>
2017-03-21 21:32:19 +0100

    added FlexLexer.h from /usr/include to be able to generate lexer on Windows with msys2 mingw

--- deps/llvm-config.cmake
+++ deps/llvm-config.cmake
@@ -1,20 +1,53 @@
-set(LLVM_ROOT "" CACHE PATH "")
-
+if(NOT LLVM_ROOT)
+message("Set LLVM_ROOT to LLVM root folder")
+endif()
 find_package(LLVM 3.9.0 PATHS ${LLVM_ROOT})
 
 if(LLVM_FOUND)
     #all works bad in Windows-> LTO-NOTFOUND 
-    llvm_map_components_to_libnames(llvm_libs analysis asmparser bitreader bitwriter codegen DebugInfoCodeView DebugInfoDWARF DebugInfoPDB Symbolize executionengine irreader Interpreter OrcJIT MCJIT RuntimeDyld Core  LibDriver LineEditor Linker MC MCDisassembler  MCParser Object ObjectYAML Option Passes ProfileData  Support  TableGen AArch64 AMDGPU ARM
-BPF
-Hexagon
-Mips
-MSP430
-NVPTX
-PowerPC
-Sparc
-SystemZ
-X86
-XCore 
+    llvm_map_components_to_libnames(llvm_libs 
+	analysis
+	asmparser
+	bitreader
+	bitwriter
+	codegen
+	DebugInfoCodeView 
+	DebugInfoDWARF 
+	DebugInfoPDB
+	Symbolize 
+	executionengine
+	irreader
+	Interpreter 
+	OrcJIT
+	MCJIT 
+	RuntimeDyld 
+	Core  
+	LibDriver 
+	LineEditor 
+	Linker
+	MC 
+	MCDisassembler 
+	MCParser 
+	Object 
+	ObjectYAML 
+	Option 
+	Passes
+	ProfileData  
+	Support  
+	TableGen 
+	AArch64 
+	AMDGPU
+	ARM
+	BPF
+	Hexagon
+	Mips
+	MSP430
+	NVPTX
+	PowerPC
+	Sparc
+	SystemZ
+	X86
+	XCore 
 )
 
     set(llvm_INCLUDE_DIRECTORIES ${LLVM_INCLUDE_DIRS})
