7f51bd030b14fcd26e35f2a127abdc570f7e2c09
Sergey Rachev <sergey.rachev@gmail.com>
2017-03-31 13:41:46 +0200

    wrap boost log under trivial API. Ugly but simple

--- deps/boost-config.cmake
+++ deps/boost-config.cmake
@@ -5,7 +5,7 @@ endif()
 #set(Boost_DEBUG ON)
 set(Boost_USE_STATIC_LIBS ON)
 
-find_package(Boost 1.60 COMPONENTS program_options system date_time thread)
+find_package(Boost 1.60 COMPONENTS system log log_setup thread date_time filesystem program_options )
 
 if(Boost_FOUND)
     set(boost_COMPILE_DEFINITIONS ${Boost_LIB_DIAGNOSTIC_DEFINITIONS})
