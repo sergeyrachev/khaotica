5f07cdf0ed1c9f96861a4731e772756f09709e27
Sergey Rachev <sergey.rachev@gmail.com>
2017-03-31 14:20:03 +0200

    gcc ignores cdecl attribute

--- src/export.cpp
+++ src/export.cpp
@@ -4,6 +4,6 @@ extern "C" double _nextbyte(double X) {
     return 123;
 }
 
-extern "C" __attribute__((cdecl)) double nextbyte(double X) {
+extern "C" double nextbyte(double X) {
     return 432;
 }
