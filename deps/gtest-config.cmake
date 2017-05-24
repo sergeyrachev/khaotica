c40d6383bc78c54b8a5bf274751ad84665cd13b3
Sergey Rachev <sergey.rachev@gmail.com>
2017-05-24 13:08:02 +0200

    install example files also don't install gtest stuff

--- deps/gtest-config.cmake
+++ deps/gtest-config.cmake
@@ -1,5 +1,5 @@
 
 set(gtest_force_shared_crt ON)
-add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/googletest)
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/googletest EXCLUDE_FROM_ALL)
 set(GTEST_LIBRARIES gmock_main)
 get_target_property(GTEST_INCLUDE_DIRECTORIES gtest INTERFACE_INCLUDE_DIRECTORIES)
