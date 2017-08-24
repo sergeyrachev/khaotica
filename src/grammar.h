48cd4eaa98834816ccbcac5202b085f45d66251d
sergey.rachev <sergey.rachev@gmail.com>
2017-08-24 16:59:59 +0200

    WIP: improving grammar to support variables, if, for, bit literals

--- src/grammar.h
+++ src/grammar.h
@@ -4,8 +4,10 @@
 #include <string>
 #include <vector>
 #include <variant>
+#include <functional>
 #include <map>
 #include <list>
+#include <memory>
 
 namespace flavor{
     struct directive_include_t{};
@@ -28,36 +30,50 @@ namespace flavor{
 
     struct variable_t;
     struct compound_t;
-    typedef std::variant<bslbf_t, uimsbf_t, tcimsbf_t, compound_t, variable_t>  symbol_t;
+    typedef std::variant<
+        bslbf_t,
+        uimsbf_t,
+        tcimsbf_t,
+        compound_t,
+        variable_t
+    >  symbol_t;
     typedef std::list<symbol_t> symbols_t;
 
-    typedef std::map<std::string, symbol_t> symbols_table_t;
-
-    struct action_t{
-
+    struct bitstring_t{
+        std::string value;
     };
 
-    struct operand_t{
-        std::variant<variable_t, value_t, >
-    };
-
-    struct expression_t {
-        action_t action;
-        operand_t operand;
-    }
-
-    struct variable_t{
-        std::string name;
-        expression_t declaration;
-    };
+    typedef uint64_t numeric_t;
+    typedef std::variant<symbol_t, numeric_t, bitstring_t> entity_t;
 
     struct compound_t{
         std::string name;
         symbols_t body;
     };
 
+    typedef std::variant<
+        std::vector<bool>,
+        uint64_t,
+        int64_t,
+        double
+    > value_t;
+
+    typedef std::variant<
+        std::logical_not<value_t>,
+        std::negate<value_t>
+    > action_t;
+
+    typedef std::pair<action_t, entity_t>  unary_expression;
+    typedef std::tuple<entity_t, action_t, entity_t>  binary_expression;
+    typedef std::variant<entity_t, unary_expression, binary_expression > expression_t;
+
+    struct variable_t{
+        std::string name;
+        std::unique_ptr<expression_t> expression;
+    };
+
     struct if_t{
-        expression_t condition;
+        std::unique_ptr<expression_t> expression;
         symbols_t _then;
         symbols_t _else;
     };
@@ -66,14 +82,8 @@ namespace flavor{
 
     };
 
-    typedef std::variant<uint64_t, int64_t, double > numeric_t;
-    typedef std::variant<
-        std::vector<bool>,
-        uint64_t,
-        numeric_t
-    > value_t;
     typedef std::list<value_t> values_t;
-
+    typedef std::map<std::string, entity_t> symbols_table_t;
 }
 
 #endif //KHAOTICA_GRAMMAR_H
