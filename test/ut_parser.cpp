5e404d4573717817137c573540e9088209e4136c
sergey.rachev <sergey.rachev@gmail.com>
2017-09-18 22:23:40 +0200

    conditions grammar fix

--- test/ut_parser.cpp
+++ test/ut_parser.cpp
@@ -19,7 +19,7 @@ namespace {
     std::string bitstring("'0011'");
 
     std::string if_( const std::string& condition ) {
-        return "if(" + condition + "){}";
+        return "root(){ if(" + condition + "){} }";
     }
     std::string unary_expression(const std::string& arg1, const std::string& op){
         return op + arg1;
@@ -30,7 +30,7 @@ namespace {
     }
 
     std::string for_(const std::string& initializer, const std::string& condition, const std::string& modifier){
-        return "for(" + initializer + ";" + condition + ";" + modifier + "){}";
+        return "root() { for(" + initializer + ";" + condition + ";" + modifier + "){} }";
     }
 
     std::string assignment(const std::string& identifier, const std::string& value){
@@ -41,9 +41,9 @@ namespace {
     std::vector<std::string> multiplicative_operators{"*", "/", "&&"};
     std::vector<std::string> compare_operators{"<", ">", "==", "<=", ">=", "!="};
     std::vector<std::string> unary_operators{"-", "~", "!"};
+    std::vector<std::string> increment{"i++", "i--"};
 
     struct interpreter_t : public ::testing::Test{
-
         std::tuple<int, flavor::document_t, flavor::symbols_t> parse(const std::string& text){
 
             std::ostringstream serr;
@@ -60,7 +60,7 @@ namespace {
 }
 
 TEST_F(interpreter_t, variable_definition){
-
+    std::vector<std::string> expressions;
     for (auto &&arg1 : {numeric_literal, string_literal}) {
         for (auto &&arg2 : {numeric_literal, string_literal}) {
             for (auto &&arg3 : {numeric_literal, string_literal}) {
@@ -68,14 +68,50 @@ TEST_F(interpreter_t, variable_definition){
                     for (auto &&mult : multiplicative_operators) {
                         for (auto &&comp: compare_operators ) {
                             auto input = binary_expression(binary_expression(arg1, add, arg2), mult, arg3);
-                            input = assignment( string_literal, binary_expression(input, comp, input));
-                            std::cout << input << std::endl;
-                            auto [ret, doc, symbols] = parse(input);
-                            EXPECT_EQ(0, ret) << input;
+                            expressions.push_back(input);
+
+                            auto definition = assignment( string_literal, binary_expression(input, comp, input));
+                            auto [ret, doc, symbols] = parse(definition);
+                            EXPECT_EQ(0, ret) << definition;
                         }
                     }
                 }
             }
         }
     }
+
+    for (auto &&expression : expressions) {
+        auto expr_if = if_(expression);
+        auto [ret, doc, symbols] = parse(expr_if);
+        EXPECT_EQ(0, ret) << expr_if;
+    }
+
+    for (auto &&init_expr : expressions) {
+        for (auto &&modifier_expr : increment) {
+            auto expr_for = for_(assignment("i", init_expr), init_expr, modifier_expr);
+            auto [ret, doc, symbols] = parse(expr_for);
+            EXPECT_EQ(0, ret) << expr_for;
+        }
+
+        auto expr_for = for_(assignment("i", init_expr), init_expr, init_expr);
+        auto [ret, doc, symbols] = parse(expr_for);
+        EXPECT_EQ(0, ret) << expr_for;
+    }
+}
+
+TEST_F(interpreter_t, conditions){
+
+    auto expr_if = R"'(
+    root(){
+        if(i) {
+            if(j) {
+            } else {
+            }
+        } else {
+            if (k) {}
+        }
+    }
+    )'";
+    auto [ret, doc, symbols] = parse(expr_if);
+    EXPECT_EQ(0, ret) << expr_if;
 }
