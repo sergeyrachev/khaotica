a05a5c6f430bb7438af4c55a81e54b0919dc7bf9
Sergey Rachev <sergey.rachev@gmail.com>
2018-03-08 16:49:14 +0100

    - WIP

--- src/mpeg2_reader.cpp
+++ src/mpeg2_reader.cpp
@@ -161,8 +161,25 @@ namespace {
         };
 
         std::shared_ptr<value_t> operator()(const sparsed_t &node) {
+
+//            auto value = bitreader.read(node.bits.length);
+//            auto it = symbols.find(node.bits.name);
+//            if (it == symbols.end()) {
+//                auto[bits, mask] = update({}, {}, value, node.range);
+//
+//                auto v = std::make_shared<value_t>(value_t{std::make_pair(node, std::make_pair(bslbf_v{value, position}, bslbf_ranged_v{bits, mask}))});
+//                symbols[node.bits.name] = v;
+//                return v;
+//            } else {
+//                auto previous = std::get<std::pair<bslbf_ranged_t, std::pair<bslbf_v, bslbf_ranged_v>>>(it->second->payload);
+//                auto previous_value = previous.second.second;
+//                auto[bits, mask] = update(previous_value.value, previous_value.mask, value, node.range);
+//
+//                auto v = std::make_shared<value_t>(value_t{std::make_pair(node, std::make_pair(bslbf_v{value, position}, bslbf_ranged_v{bits, mask}))});
+//                symbols[node.bits.name] = v;
+//            }
             return {};
-        };
+        }
 
         std::shared_ptr<value_t> operator()(const reference_t &node) {
             auto it = doc.global->definitions.find(node.name);
@@ -311,236 +328,6 @@ namespace {
             return nullptr;
         };
 
-
-//        std::shared_ptr<value_t> operator()(const bslbf_t &node) {
-//            auto position = bitreader.position();
-//            auto value = bitreader.read(node.length);
-//            auto v = std::make_shared<value_t>(value_t{std::make_pair(node, bslbf_v{value, position})});
-//            symbols[node.name] = v;
-//            return v;
-//        };
-//
-//        std::shared_ptr<value_t>operator()(const std::vector<uimsbf_t>& node )  {
-//            return nullptr;
-//        }
-//
-//        std::shared_ptr<value_t> operator()(const sparsed_t &node) {
-//
-//            auto value = bitreader.read(node.bits.length);
-//
-//            auto it = symbols.find(node.bits.name);
-//            if( it == symbols.end()){
-//                auto [bits, mask] = update({}, {}, value, node.range);
-//
-//                auto v = std::make_shared<value_t>(value_t{std::make_pair(node, std::make_pair(bslbf_v{value, position}, bslbf_ranged_v{bits, mask}))});
-//                symbols[node.bits.name] = v;
-//                return v;
-//            } else {
-//                auto previous = std::get<std::pair<bslbf_ranged_t, std::pair<bslbf_v, bslbf_ranged_v>>>(it->second->payload);
-//                auto previous_value = previous.second.second;
-//                auto [bits, mask] = update(previous_value.value, previous_value.mask, value, node.range);
-//
-//                auto v = std::make_shared<value_t>(value_t{std::make_pair(node, std::make_pair(bslbf_v{value, position}, bslbf_ranged_v{bits, mask}))});
-//                symbols[node.bits.name] = v;
-//                return v;
-//            }
-//        };
-//
-//        std::shared_ptr<value_t> operator()(const uimsbf_t &node) {
-//            auto position = bitreader.position();
-//            auto value = algorithm::to_ull_msbf(bitreader.read(node.length));
-//            auto v = std::make_shared<value_t>(value_t{std::make_pair(node, uimsbf_v{value, position})});
-//            symbols[node.name] = v;
-//            return v;
-//        };
-//
-//        std::shared_ptr<value_t> operator()(const bitstring_t &node) {
-//            auto value = algorithm::unpack(node.value);
-//            return std::make_shared<value_t>(value_t{std::make_pair(expression_t{node}, value)});
-//        };
-//
-//        std::shared_ptr<value_t> operator()(const integer_t &node) {
-//            auto value = node.value;
-//            return std::make_shared<value_t>(value_t{std::make_pair(expression_t{node}, value)});
-//        };
-//
-//        std::shared_ptr<value_t> operator()(const identifier_t &node) {
-//
-//            auto symbol = symbols.find(node.name);
-//            if (symbol != symbols.end()) {
-//                expression_v value = extract(*symbol->second);
-//                auto v = std::make_shared<value_t>(value_t{std::make_pair(expression_t{node}, value)});
-//                return v;
-//            }
-//
-//            auto definition = lookup(node.name, scope);
-//            if (definition) {
-//                auto value = on(definition);
-//                return value;
-//            }
-//
-//            // FIXME: Fix this shit with a correct scope handling. Note! WHAT is a scope in bitstream is not clear yet.
-//            definition = deep_lookup(node.name, scope);
-//            if(definition){
-//                auto value = std::visit(default_t(), definition->payload);
-//                return std::make_shared<value_t>(value_t{std::make_pair(expression_t{node}, value)});
-//            }
-//
-//            assert(false && "It is REALLY bad to be there and try to use unresolved symbol");
-//            return nullptr;
-//        };
-//
-//        std::shared_ptr<value_t> operator()(const reference_t &node) {
-//            std::list<std::shared_ptr<value_t>> value;
-//
-//            auto it = doc.definitions.find(node.name);
-//            assert(it != doc.definitions.end());
-//            return on(it->second);
-//        };
-//
-//        std::shared_ptr<value_t> operator()(const if_t &node) {
-//            scope = node.scope;
-//
-//            auto conditional_expression = on(node.condition);
-//            auto conditional_expression_payload = std::get<std::pair<expression_t, expression_v>>(conditional_expression->payload);
-//            auto conditional_expression_value = std::visit(conditional_t(), conditional_expression_payload.second);
-//
-//            std::list<std::shared_ptr<value_t>> value;
-//            if (conditional_expression_value) {
-//                for (auto &&item : node._then) {
-//                    value.push_back(on(item));
-//                }
-//            } else {
-//                for (auto &&item : node._else) {
-//                    value.push_back(on(item));
-//                }
-//            }
-//
-//            scope = node.scope->parent;
-//            return std::make_shared<value_t>(value_t{std::make_pair(node, if_v{ conditional_expression_value, value} )});
-//        };
-//
-//        std::shared_ptr<value_t> operator()(const for_t &node) {
-//
-//            scope = node.scope;
-//
-//            if(node.initializer){
-//                auto initializer = on(*node.initializer);
-//            }
-//
-//            bool conditional_expression_value(true);
-//            if(node.condition){
-//                auto conditional_expression = on(*node.condition);
-//                auto conditional_expression_payload = std::get<std::pair<expression_t, expression_v>>(conditional_expression->payload);
-//                conditional_expression_value = std::visit(conditional_t(), conditional_expression_payload.second);
-//            }
-//
-//            std::vector<loop_v::iteration_t> value;
-//            while(conditional_expression_value){
-//
-//                loop_v::iteration_t iteration;
-//                for (auto &&item : node.body) {
-//                    iteration.push_back(on(item));
-//                }
-//                value.emplace_back(std::move(iteration));
-//
-//                if(node.modifier){
-//                    auto modifier = on(*node.modifier);
-//                }
-//
-//                if(node.condition){
-//                    auto conditional_expression = on(*node.condition);
-//                    auto conditional_expression_payload = std::get<std::pair<expression_t, expression_v>>(conditional_expression->payload);
-//                    conditional_expression_value = std::visit(conditional_t(), conditional_expression_payload.second);
-//                }
-//            }
-//            scope = node.scope->parent;
-//            return std::make_shared<value_t>(value_t{std::make_pair(node, loop_v{value} )});
-//        };
-//
-//        std::shared_ptr<value_t> operator()(const do_t &node) {
-//
-//        }
-//
-//        std::shared_ptr<value_t> operator()(const while_t &node) {
-//
-//        }
-//
-//        std::shared_ptr<value_t> operator()(const compound_t &node) {
-//            scope = node.scope;
-//
-//            std::list<std::shared_ptr<value_t>> value;
-//            for (auto &&item : node.body) {
-//                value.push_back(on(item));
-//            }
-//            scope = node.scope->parent;
-//            return std::make_shared<value_t>(value_t{std::make_pair(node, value)});
-//        };
-//
-//        std::shared_ptr<value_t> operator()(const assignment_t &node) {
-//            auto initializer = on(node.expression);
-//            symbols[node.symbol] = initializer;
-//            auto initializer_payload = std::get<std::pair<expression_t, expression_v>>(initializer->payload);
-//            return std::make_shared<value_t>(value_t{std::make_pair(expression_t{node}, expression_v{initializer_payload.second})});
-//        };
-//
-//        std::shared_ptr<value_t> operator()(const preincrement_t &node) {
-//            auto previous_value = symbols.find(node.operand)->second;
-//            auto& previous_payload = std::get<std::pair<expression_t, expression_v>>(previous_value->payload);
-//            auto value = std::visit(find_binary_functor(node.operation), previous_payload.second, expression_v{int64_t{1}});
-//
-//            previous_payload.second = value;
-//            return std::make_shared<value_t>(value_t{std::make_pair(expression_t{node}, value)});
-//        };
-//
-//        std::shared_ptr<value_t> operator()(const postincrement_t &node) {
-//            auto previous_value = symbols.find(node.operand)->second;
-//            auto& previous_payload = std::get<std::pair<expression_t, expression_v>>(previous_value->payload);
-//            auto value = std::visit(find_binary_functor(node.operation), previous_payload.second, expression_v{int64_t{1}});
-//
-//            previous_payload.second = value;
-//            return previous_value;
-//        };
-//
-//
-//        std::shared_ptr<value_t> operator()(const unary_expression_t &node) {
-//            auto operand = on(node.operand);
-//
-//            auto operand_payload = std::get<std::pair<expression_t, expression_v>>(operand->payload);
-//
-//            auto value = std::visit(find_unary_functor(node.operation), operand_payload.second);
-//            return std::make_shared<value_t>(value_t{std::make_pair(expression_t{node}, expression_v{value})});
-//        };
-//
-//        std::shared_ptr<value_t> operator()(const binary_expression_t &node) {
-//            auto left = on(node.left_operand);
-//            auto right = on(node.right_operand);
-//
-//            auto left_payload = std::get<std::pair<expression_t, expression_v>>(left->payload);
-//            auto right_payload = std::get<std::pair<expression_t, expression_v>>(right->payload);
-//
-//            auto value = std::visit(find_binary_functor(node.operation), left_payload.second, right_payload.second);
-//            return std::make_shared<value_t>(value_t{std::make_pair(expression_t{node}, expression_v{value})});
-//        };
-//
-//        std::shared_ptr<value_t> operator()(const position_t &node) {
-//
-//            uint64_t value(0);
-//            if( node.name){
-//                auto symbol = symbols.find(*node.name);
-//                if (symbol != symbols.end()) {
-//                    value = extract_location(*symbol->second);
-//                }
-//            } else {
-//                value = bitreader.position();
-//            }
-//
-//            return std::make_shared<value_t>(value_t{std::make_pair(expression_t{node}, expression_v{value})});
-//        };
-//
-//        std::shared_ptr<value_t> operator()(const nextbits_t &node) {
-//            return nullptr;
-//        }
     private:
         expression_v extract(const value_t &value) {
             if( const auto&& val = std::get_if<std::pair<bslbf_t, bitstring_v>>(&value.payload)){
